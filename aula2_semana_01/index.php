a922a0f43dc6f1333780d64f7ebfe9eaf1994ee4
Henrique Douglas <henriquedouglas2803@gmail.com>
2023-09-26 20:46:13 -0300

    adiciona correção do nome da pasta

---
+++ aula2_semana_01/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indíces</title>
+</head>
+<body>
+        <p><a href="carta_ferias.php">Solução exercício carta férias</a></p>  
+</body>
+</html>
