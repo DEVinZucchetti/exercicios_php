16df81eaf4b354aa2acd9e762c72a9b7117b4d65
Henrique Douglas <henriquedouglas2803@gmail.com>
2023-09-28 20:20:04 -0300

    adiciona exemplo post

--- aula3_semana_01/variaveis_super_globais.php
+++ aula3_semana_01/variaveis_super_globais.php
@@ -1,6 +1,9 @@
 <?php
-header('Content-Type: application/json');
+header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
+
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -12,8 +15,22 @@ if ($method === 'GET') {
     echo json_encode(['products' => $data]);
 } else if ($method === "POST") {
     $body = file_get_contents("php://input");
-    $data = json_decode($body);
+    $data = json_decode($body); // acesso ao body da requisicao
 
-    
+    if (isset($data->nome) === false || empty($data->nome)) {
+        http_response_code(400);
+        echo json_encode(['error' => 'O nome é obrigatório']);
+        exit;
+    }
+
+    // ler arquivo
+    $text = file_get_contents('database.txt'); // "[]"
+    $arrayItems = json_decode($text); // []
+
+    $arrayItems[] = $data;
+
+    file_put_contents('database.txt', json_encode($arrayItems));
+
+    http_response_code(201);
     echo json_encode(['message' => 'Recebi um post']);
 }
